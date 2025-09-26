dfdf7af7cd48a778069072469b4f6ac3867a3333
kevinn.s <vinn1864.s@gmail.com>
2025-09-26 23:38:05 +0700

    w

--- resources/views/components/app/header.blade.php
+++ resources/views/components/app/header.blade.php
@@ -1,18 +1,17 @@
 <header
-    class="text-gray-base sticky top-0 before:absolute before:inset-0 before:backdrop-blur-md max-lg:before:bg-white/90 before:-z-10 z-30 {{ $variant === 'v2' || $variant === 'v3' ? 'before:bg-white before:absolute before:h-px before:inset-x-0 before:top-full before:bg-gray-200 before:-z-10' : 'max-lg:shadow-xs lg:before:bg-gray-100/90' }} {{ $variant === 'v2' ? 'dark:before:bg-gray-800' : '' }} {{ $variant === 'v3' ? 'dark:before:bg-gray-900' : '' }}">
+    class="text-gray-base sticky top-0 z-30 bg-white ">
     <div class="relative" x-data="{ tentangHover: false }">
     <div
-        class="px-4 sm:px-6 lg:px-14 pb-5 {{ $variant === 'v2' || $variant === 'v3' ? '' : 'lg:border-b border-gray-200 border-t-8 border-t-green-950' }}">
+        class="px-4 sm:px-6 lg:px-14 {{ $variant === 'v2' || $variant === 'v3' ? '' : 'lg:border-b border-gray-200 border-t-10 border-t-[var(--primary-green)]' }}">
         <div class="flex items-center justify-between ">
 
             <!-- Header: Left side -->
-            <div class="flex">
-
-                <div class="h-full w-[64px] md:w-[80px] md:h-[80px] flex flex-col justify-center icon-drm">
+            <div class="flex h-full">
+                <div class="w-[64px] md:w-[80px] md:h-[80px] flex flex-col justify-center icon-drm">
                    
                 </div>
                 <!-- Hamburger button -->
-                <button class="text-gray-500 hover:text-gray-600 lg:hidden"
+                <!-- <button class="text-gray-500 hover:text-gray-600 lg:hidden"
                     @click.stop="sidebarOpen = !sidebarOpen" aria-controls="sidebar" :aria-expanded="sidebarOpen">
                     <span class="sr-only">Open sidebar</span>
                     <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -20,11 +19,11 @@
                         <rect x="4" y="11" width="16" height="2" />
                         <rect x="4" y="17" width="16" height="2" />
                     </svg>
-                </button>
+                </button> -->
             </div>
 
             <!-- Header: Right side -->
-            <div class="">
+            <div class="pb-5">
                 
                 <div class="flex items-center justify-end gap-6 py-2  text-sm">
                     @guest
@@ -62,9 +61,9 @@
         </div>
     </div>
     <div 
-        class="absolute w-full h-28 before:border-b-2 before:border-b-gray-200 before:absolute before:inset-0 before:backdrop-blur-md max-lg:before:bg-white/90 before:-z-10 z-30 {{ $variant === 'v2' || $variant === 'v3' ? 'before:bg-white before:absolute before:h-px before:inset-x-0 before:top-full before:bg-gray-200 before:-z-10' : 'max-lg:shadow-xs lg:before:bg-gray-100/90' }} {{ $variant === 'v2' ? 'dark:before:bg-gray-800' : '' }} {{ $variant === 'v3' ? 'dark:before:bg-gray-900' : '' }} [clip-path:inset(0_3.5rem)] "
-        x-show="tentangHover"
+        class="bg-white absolute w-full h-28 ml-14 [clip-path:inset(0_7.0rem_0_0)]"
         x-transition.opacity
+        x-show="tentangHover"
     >
     </div>
     </div>
